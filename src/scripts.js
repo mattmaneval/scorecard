9927b882f22064fa39644eece6c4fec3e64fcbc6
mattmaneval <manevalmusic@gmail.com>
2021-01-24 17:13:37 -0500

    functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,56 +1,46 @@
+const items = Array.from(document.querySelectorAll(`input[type="number"]`));
 const calculateFrontButton = document.querySelector('.calculate-front');
 const calculateBackButton = document.querySelector('.calculate-back');
 const calculateTotalButton = document.querySelector('.calculate-total');
-const inputs = document.querySelectorAll('.hole input');
 
-const calculateFront = () => {
-  const hole1 = document.getElementById('1');
-  const hole2 = document.getElementById('2');
-  const hole3 = document.getElementById('3');
-  const hole4 = document.getElementById('4');
-  const hole5 = document.getElementById('5');
-  const hole6 = document.getElementById('6');
-  const hole7 = document.getElementById('7');
-  const hole8 = document.getElementById('8');
-  const hole9 = document.getElementById('9');
+const frontScore = document.querySelector('.front-nine h4');
+const backScore = document.querySelector('.back-nine h4');
+const combinedScore = document.querySelector('.score h4');
 
-  const frontTotal = Number(hole1.value) + Number(hole2.value) + Number(hole3.value)
-    + Number(hole4.value) + Number(hole5.value) + Number(hole6.value)
-    + Number(hole7.value) + Number(hole8.value) + Number(hole9.value);
+function calculate(e) {
+	e.preventDefault();
 
-  displayTotal = document.querySelector('.front-nine-total');
-  displayTotal.textContent = frontTotal;
-  console.log(frontTotal);
-  return frontTotal;
+	if(e.target === calculateFrontButton) {
+		const getFront = items
+			.filter(item => item.id <= 9)
+			.map(item => parseInt(item.value))
+			.reduce((accumulator, currentValue) => accumulator + currentValue);
+		// console.log(getFront);
+		return getFront > 0 
+		? frontScore.textContent = `Front Nine Score: ${getFront}`
+		: frontScore.textContent = `Enter your scores`;
+	} else if(e.target === calculateBackButton) {
+		const getBack = items
+			.filter(item => item.id > 9)
+			.map(item => parseInt(item.value))
+			.reduce((accumulator, currentValue) => accumulator + currentValue);
+		// console.log(getBack);
+		
+
+		return getBack > 0 
+		? backScore.textContent = `Back Nine Score: ${getBack}`
+		: backScore.textContent = `Enter your scores`;
+	} else {
+		totalScore = items
+		.map(item => parseInt(item.value))
+		.reduce((accumulator, currentValue) => accumulator + currentValue);
+		// console.log(totalScore);
+		return totalScore > 0
+		? combinedScore.textContent = `18 Hole Score: ${totalScore}`
+		: combinedScore.textContent = `Enter your scores for front and back nines`;
+	}
 }
 
-const calculateBack = () => {
-  const hole10 = document.getElementById('10');
-  const hole11 = document.getElementById('11');
-  const hole12 = document.getElementById('12');
-  const hole13 = document.getElementById('13');
-  const hole14 = document.getElementById('14');
-  const hole15 = document.getElementById('15');
-  const hole16 = document.getElementById('16');
-  const hole17 = document.getElementById('17');
-  const hole18 = document.getElementById('18');
-
-  const backTotal = Number(hole10.value) + Number(hole11.value) + Number(hole12.value)
-    + Number(hole13.value) + Number(hole14.value) + Number(hole15.value)
-    + Number(hole16.value) + Number(hole17.value) + Number(hole18.value);
-
-  displayTotal = document.querySelector('.back-nine-total');
-  displayTotal.textContent = backTotal;
-  console.log(backTotal);
-  return backTotal;
-}
-
-const calculateTotal = () => {
-  displayTotal = document.querySelector('.score-combined');
-  displayTotal.textContent = calculateFront() + calculateBack();
-  console.log(calculateFront() + calculateBack());
-}
-
-calculateFrontButton.addEventListener('click', calculateFront);
-calculateBackButton.addEventListener('click', calculateBack);
-calculateTotalButton.addEventListener('click', calculateTotal);
+calculateFrontButton.addEventListener('click', calculate);
+calculateBackButton.addEventListener('click', calculate);
+calculateTotalButton.addEventListener('click', calculate);
